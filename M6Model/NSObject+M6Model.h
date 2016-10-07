9c78a66484f72bb7ff23a48e7100cbd701387f9d
chenms-m2 <hellochenms@163.com>
2016-10-07 18:21:45 +0800

    Feature: Init. BugId:/ Description:

---
+++ M6Model/NSObject+M6Model.h
@@ -0,0 +1,19 @@
+//
+//  NSObject+M6Model.h
+//  M6Model
+//
+//  Created by chenms on 16/10/7.
+//  Copyright © 2016年 chenms.m2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (M6Model)
++ (instancetype)m6_modelFromJSON:(NSDictionary *)json;
+- (NSDictionary *)m6_toJSON;
+@end
+
+@interface NSArray (M6Model)
++ (NSArray *)m6_modelArrayOfClass:(Class)clas fromJSON:(NSArray *)json;
+- (NSArray *)m6_toJSON;
+@end
---
+++ M6Model/NSObject+M6Model.m
@@ -0,0 +1,31 @@
+//
+//  NSObject+M6Model.m
+//  M6Model
+//
+//  Created by chenms on 16/10/7.
+//  Copyright © 2016年 chenms.m2. All rights reserved.
+//
+
+#import "NSObject+M6Model.h"
+
+@implementation NSObject (M6Model)
++ (instancetype)m6_modelFromJSON:(NSDictionary *)json {
+    return nil;
+}
+- (NSDictionary *)m6_toJSON {
+    return nil;
+}
+
+@end
+
+
+@implementation NSArray (M6Model)
+
++ (NSArray *)m6_modelArrayOfClass:(Class)clas fromJSON:(NSArray *)json {
+    return nil;
+}
+- (NSArray *)m6_toJSON {
+    return nil;
+}
+
+@end
